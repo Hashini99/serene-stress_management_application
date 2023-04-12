950bb6b7fa161c6040ca3b0a135bb665ea210a81
Hashini99 <bhhrkumari@students.nsbm.ac.lk>
2023-04-13 00:45:42 +0530

    expert option done

---
+++ app/src/main/java/com/example/serene/expert_support/ExpertList.kt
@@ -0,0 +1,88 @@
+package com.example.serene.expert_support
+
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
+import android.os.Bundle
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.serene.*
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_expert_list_view.*
+import kotlinx.android.synthetic.main.activity_journal_main.*
+
+data class Expert(
+    // val datetime:Date,
+    val name: String = "",
+)
+class  ExpertViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+class ExpertList: AppCompatActivity() {
+    val db = Firebase.firestore
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expert_list_view)
+        supportActionBar?.title = getString(R.string.journal_main)
+
+        val query = db.collection("expert")
+        val options = FirestoreRecyclerOptions.Builder<Expert>().setQuery(query, Expert::class.java)
+            .setLifecycleOwner(this).build()
+        val adapter = object : FirestoreRecyclerAdapter<Expert, ExpertViewHolder>(options) {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExpertViewHolder {
+                val view = LayoutInflater.from(this@ExpertList)
+                    // .inflate(android.R.layout.simple_list_item_2, parent, false)
+                    .inflate(R.layout.activity_expert_card, parent, false)
+                return ExpertViewHolder(view)
+            }
+            override fun onBindViewHolder(holder: ExpertViewHolder, position: Int, model: Expert) {
+
+                val tvName: TextView = holder.itemView.findViewById(R.id.item_tv_name)
+//                val tvDate: TextView = holder.itemView.findViewById(R.id.dateTextView)
+//                val tvDescription: TextView = holder.itemView.findViewById(R.id.descTextView)
+
+             //   tvDate.text = model.date
+                tvName.text = model.name
+             //   tvDescription.text = model.description
+
+//                tvDate.setTextColor(Color.parseColor("#FFFFFF"))
+//                tvDate.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12F)
+//
+//                tvTitle.typeface = Typeface.defaultFromStyle(Typeface.BOLD)
+//                tvDescription.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14F)
+
+
+
+
+                val documentId = snapshots.getSnapshot(position).id
+
+                holder.itemView.setOnClickListener {
+                    changePageToExpertSingleView(documentId)
+                }
+            }
+        }
+        expertRecyclerView.adapter = adapter
+        expertRecyclerView.layoutManager = LinearLayoutManager(this)
+    }
+    fun changePageToExpertSingleView(docID: String){
+        val intent = Intent(this, ExpertSingleView::class.java)
+        intent.putExtra("docID", docID)
+        startActivity(intent)
+    }
+//    fun addNewJournal(view: View){
+//        startActivity(Intent(this, JournalAddEditNoteActivity::class.java))
+//        finish()
+//    }
+}
+
+
+
+
---
+++ app/src/main/java/com/example/serene/expert_support/ExpertSingleView.kt
@@ -0,0 +1,82 @@
+package com.example.serene.expert_support
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.serene.JournalMainActivity
+import com.example.serene.JournalViewHolder
+import com.example.serene.Note
+import com.example.serene.R
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_journal_single_view.*
+import kotlinx.android.synthetic.main.expert_single_view.*
+
+class ExpertSingleView : AppCompatActivity() {
+
+    lateinit var documentID: String
+    lateinit var decription: String
+    lateinit var name: String
+    //var client_id: String = "ARKL7p7RWGWudGaKC4KIjPhAd46IzO8Jl61jfgiEIMKxO3-JirB0te6vR-v_QFk9mYz1bAq00AJ9rqze"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
+
+        setContentView(R.layout.expert_single_view)
+
+        documentID = intent.getStringExtra("docID").toString()
+
+        FirebaseFirestore.getInstance().collection("expert").document(documentID.toString()).get()
+            .addOnSuccessListener {
+
+
+
+                name = it.data!!.getValue("name").toString()
+                decription = it.data!!.getValue("qulification").toString()
+
+
+//                txt_date.text = it.data!!.getValue("date").toString()
+//                txt_time.text = it.data!!.getValue("time").toString()
+                txt_name.text = it.data!!.getValue("name").toString()
+                desc.text = it.data!!.getValue("qulification").toString()
+
+                //descriptionVisibility()
+
+            }.addOnFailureListener{
+                Toast.makeText(applicationContext,"Cannot Get Data from Server", Toast.LENGTH_LONG).show()
+            }
+
+    }
+
+    override fun onDestroy() {
+        //stopService(Intent(this, PayPalService::class.java))
+        super.onDestroy()
+    }
+//    fun descriptionVisibility(){
+//        if(decription == "" || decription == null){
+//            txt_description.setVisibility(View.GONE)
+//            lbl_description.setVisibility(View.GONE)
+//        }
+//    }
+
+
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        startActivity(Intent(this, ExpertList::class.java))
+        finish()
+    }
+}
